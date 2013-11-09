05a951dc11efe190b84d9f3e2b926dcbc9a9cc6c
Kristoffer Walker <kris@kixx.name>
2013-11-09 00:25:41 -0500

    Hook up URL filter Transform stream.
    	modified:   index.js

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-var ntwitter = require('ntwitter');
-var config = require('./config.json')
+var ntwitter = require('ntwitter')
+  , URLFilter = require('./lib/url_filter').URLFilter
+  , CONFIG = require('./config.json')
 
-var twitter = new ntwitter(config);
 
-twitter.stream('statuses/sample', function(stream) {
-  stream.on('data', function (data) {
-    console.log(data);
+function main() {
+  openTwitterStream(function (twitterStream) {
+    onTwitterData(twitterStream, printUpdate);
   });
-});
+}
+
+function onTwitterData(twitterStream, callback) {
+  var filter = URLFilter.create()
+
+  filter.on('data', callback);
+
+  twitterStream.on('data', function (updateStruct) {
+    filter.write(JSON.stringify(updateStruct));
+  });
+}
+
+function openTwitterStream(callback) {
+  var twitter = new ntwitter(CONFIG);
+  twitter.stream('user', callback);
+}
+
+function printUpdate(updateString) {
+  var update = JSON.parse(updateString)
+
+  console.log('-------------------------------------------------------------');
+  console.log(update.user.name, '('+ update.user.screen_name +')');
+  console.log(update.created_at);
+  console.log(update.text);
+
+  console.log(' -- links ->');
+  update.entities.urls.forEach(function (url) {
+    console.log(url.expanded_url);
+  });
+}
+
+main();
